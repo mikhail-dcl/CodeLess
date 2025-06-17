b5a2a725998a293f2b64ab0f79c00d83f89425d6
Mikhail Agapov <mikhail.agapov@decentraland.org>
2025-06-17 17:32:13 +0300

    Include the combined test

---
+++ CodeLess.Interfaces.Tests/Combined/AllFunctionalities.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodeLess.Interfaces.Tests
+{
+    public class Compilation
+    {
+        [TestFixture]
+        public class Combined
+        {
+            private const string TEST_DIR = nameof(Combined);
+
+            [Test]
+            public Task AllFunctionalities() =>
+                InterfaceVerifier.RunAsync(TEST_DIR, nameof(AllFunctionalities));
+        }
+    }
+}
